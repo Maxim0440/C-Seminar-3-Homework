563dd76a7b1db2a5b829e6f5629a86f10c1c8bba
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-12 12:36:57 +0300

    Task19.задача наопределения полиандрома

---
+++ Task19/Program.cs
@@ -0,0 +1,40 @@
+﻿ 
+// Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+
+//int num = new Random().Next(10000,100000);
+//System.Console.WriteLine($"num = {num}");
+
+System.Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num / 10000;
+int num2 = num / 1000 - num1 * 10;
+int num5 = num % 10;
+int num4 = (num % 100 - num5) / 10;
+
+
+if (num1 == num5 && num2 == num4)
+
+{
+    System.Console.Write($"{num} -> да");
+}
+else if (num > 99999)
+{
+    System.Console.Write($"{num} -> некорректное число");
+}
+else if (num < 10000)
+{
+    System.Console.Write($"{num} -> некорректное число");
+}
+
+else
+{
+    System.Console.Write($"{num} -> нет");
+}   
